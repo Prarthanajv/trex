bf04363e3d63e66bb1b48d57abab0d230ee1bd2e
Prarthanajv <98090337+Prarthanajv@users.noreply.github.com>
2022-03-07 14:01:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg=loadImage("sea.png");
+  shipImg=loadImage("ship-1.png");
+
+
+}
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+}
+sea=createSprite(400,200);
+sea.addImage(seaImage);
+s
+ship.addImage
+
+
+function draw() {
+  background("blue");
+ 
+}
